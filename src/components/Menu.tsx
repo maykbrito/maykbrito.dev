a4867fc1b77ea94a455e7e4a312e0577904f4023
Mayk Brito <maykbrito@gmail.com>
2023-12-29 19:31:24 -0300

    simplify home and blog

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,87 +1,28 @@
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 
-import Me from '@/components/Me'
-
 export default function NavigationMenuDemo() {
+  const isActive = (path: string) => location.pathname == path
+
   return (
     <NavigationMenu>
       <NavigationMenuList>
       <NavigationMenuItem>
-            <NavigationMenuLink href="/" className={navigationMenuTriggerStyle()}>
+            <NavigationMenuLink href="/" active={isActive("/")} className={navigationMenuTriggerStyle()}>
               Home
             </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Sobre</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <div
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md text-center"
-                  >
-                    <Me className="mx-auto" />
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      @maykbrito
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                    😍 Apaixonado por ensino.<br/>
-⭐️ Descomplicando a Programação WEB.<br/>
-💡 Gosto de falar sobre hard e soft skills.<br/>
-                    </p>
-                  </div>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="https://discover.maykbrito.dev" title="Discover">
-                Curso gratuido de programação WEB
-              </ListItem>
-              <ListItem href="https://rocketseat.com.br" title="Rocketseat">
-                Formação completa em programação WEB.
-              </ListItem>
-              <ListItem href="https://seo.maykbrito.dev" title="SEO para Devs">
-                Curso de SEO para Devs
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
+            <NavigationMenuLink href="/blog" active={isActive("/blog")} className={navigationMenuTriggerStyle()}>
+              Blog
+            </NavigationMenuLink>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
   )
 }
-
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  )
-})
-ListItem.displayName = "ListItem"
