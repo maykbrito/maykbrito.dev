be3dd5ad01db42dde59ef93d8f99a0ab8fdc07d5
Mayk Brito <maykbrito@gmail.com>
2023-12-29 19:30:55 -0300

    fix: persists dark light theme

--- src/components/ModeToogle.tsx
+++ src/components/ModeToogle.tsx
@@ -3,30 +3,23 @@ import { Moon, Sun } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 
-export function ModeToggle() {
-  const [theme, setThemeState] = React.useState<
-    "theme-light" | "dark" | "system"
-  >("theme-light")
+export function ModeToggle({...props}) {
+  const initialState = localStorage.getItem('theme') ?? 'dark'
+  const [theme, setThemeState] = React.useState(initialState)
 
   React.useEffect(() => {
-    const isDarkMode = document.documentElement.classList.contains("dark")
-    setThemeState(isDarkMode ? "dark" : "theme-light")
-  }, [])
-
-  React.useEffect(() => {
-    const isDark =
-      theme === "dark" ||
-      (theme === "system" &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    document.documentElement.classList[isDark ? "add" : "remove"]("dark")
+    document.documentElement.classList[theme === 'dark' ? "add" : "remove"]("dark")
+    localStorage.setItem('theme', theme)
   }, [theme])
 
   return (
-    <Button variant="outline" size="icon"
-      onClick={() => setThemeState(theme === "theme-light" ? "dark" : "theme-light")}>
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle mode</span>
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-    </Button>
+    <div {...props}>      
+      <Button variant="outline" size="icon"
+        onClick={() => setThemeState(theme === "light" ? "dark" : "light")}>
+        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <span className="sr-only">Toggle mode</span>
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      </Button>
+    </div>
   )
 }
