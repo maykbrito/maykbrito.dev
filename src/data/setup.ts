1775de93993ebdbd521bc191c44cff9efb10f65f
Mayk Brito <maykbrito@gmail.com>
2024-02-20 23:41:53 -0300

    add amazon afiliate link

--- src/data/setup.ts
+++ src/data/setup.ts
@@ -1,73 +1,73 @@
 export default [
   {
-    name: "KZ ZS10 Pro",
+    name: "Fone KZ ZS10 Pro",
     imgSrc:
       "https://m.media-amazon.com/images/I/61-0DcVDsmL.__AC_SX300_SY300_QL70_ML2_.jpg",
-    url: "https://www.amazon.com.br/Linsoul-KZ-ZS10-Pro-intra-auricular/dp/B07QKT3BKR",
+    url: "https://www.amazon.com.br/Linsoul-KZ-ZS10-Pro-intra-auricular/dp/B07QKT3BKR?&_encoding=UTF8&tag=maykbrito0a-20&linkCode=ur2&linkId=b704e084d0a1d5e6921818da55023eb5&camp=1789&creative=9325",
   },
   {
-    name: "Camera Panasonic Lumix DC-GH5",
+    name: "Camera Panasonic Lumix GH5",
     imgSrc:
       "https://m.media-amazon.com/images/I/61lsuhBWUJL.__AC_SX300_SY300_QL70_ML2_.jpg",
-    url: "https://www.amazon.com.br/Camera-Panasonic-DC-GH5-Digital-Espelho/dp/B01MZ3LQQ5/",
+    url: "https://www.amazon.com.br/Camera-Panasonic-DC-GH5-Digital-Espelho/dp/B01MZ3LQQ5/?&_encoding=UTF8&tag=maykbrito0a-20&linkCode=ur2&linkId=6e0839a07359933802ecdcc6a77151cb&camp=1789&creative=9325",
   },
   {
-    name: "SIGMA Lente Dc, 18-35 Mm, F/1.8 Art",
+    name: "Lente Sigma Art 18-35 1.8",
     imgSrc:
       "https://m.media-amazon.com/images/I/514hJGqTi+L._AC_SY300_SX300_.jpg",
-    url: "https://www.amazon.com.br/Lente-Sigma-18-35mm-para-Nikon/dp/B00DBL09FG/",
+    url: "https://www.amazon.com.br/Lente-Sigma-18-35mm-para-Nikon/dp/B00DBL09FG/?&_encoding=UTF8&tag=maykbrito0a-20&linkCode=ur2&linkId=0fc38b29551b4c76549197144941d757&camp=1789&creative=9325",
   },
   {
-    name: "Elgato Stream Deck 15 teclas",
+    name: "Elgato Stream Deck",
     imgSrc:
       "https://m.media-amazon.com/images/I/61SePZyhVaL.__AC_SX300_SY300_QL70_ML2_.jpg",
-    url: "https://www.amazon.com.br/Elgato-Stream-Controlador-Cria%C3%A7%C3%A3o-Conte%C3%BAdo/dp/B06XKNZT1P/",
+    url: "https://www.amazon.com.br/Elgato-Stream-Controlador-Cria%25C3%25A7%25C3%25A3o-Conte%25C3%25BAdo/dp/B06XKNZT1P/?&_encoding=UTF8&tag=maykbrito0a-20&linkCode=ur2&linkId=e7883d16b36d2408188f99eabe8e2925&camp=1789&creative=9325",
   },
   {
     name: "Key Light Air Elgato",
     imgSrc:
       "https://m.media-amazon.com/images/I/41NU7dMH8YL.__AC_SX300_SY300_QL70_ML2_.jpg",
-    url: "https://www.amazon.com.br/Key-Light-Air-Elgato-10LAB9901/dp/B082QHRZFW/",
+    url: "https://www.amazon.com.br/Key-Light-Air-Elgato-10LAB9901/dp/B082QHRZFW/?&_encoding=UTF8&tag=maykbrito0a-20&linkCode=ur2&linkId=84450df397fdb2e433cf8bb7f86aa24b&camp=1789&creative=9325",
   },
   {
     name: "Focusrite SCARLETT SOLO Interface de Áudio",
     imgSrc:
       "https://m.media-amazon.com/images/I/613veRuWtmL.__AC_SY300_SX300_QL70_ML2_.jpg",
-    url: "https://www.amazon.com.br/Interface-audio-SCARLETT-SOLO-FOCUSRITE/dp/B07QR6Z1JB/",
+    url: "https://www.amazon.com.br/Interface-audio-SCARLETT-SOLO-FOCUSRITE/dp/B07QR6Z1JB/?&_encoding=UTF8&tag=maykbrito0a-20&linkCode=ur2&linkId=0d04e441e499b289c663c13479298f23&camp=1789&creative=9325",
   },
   {
     name: "Microfone Behringer XM1800S",
     imgSrc: "https://m.media-amazon.com/images/I/710zDNfn3FL.__AC_SY300_SX300_QL70_ML2_.jpg",
-    url: "https://www.amazon.com.br/Behringer-XM1800S-Microfone/dp/B000NJ2TIE/",
+    url: "https://www.amazon.com.br/Behringer-XM1800S-Microfone/dp/B000NJ2TIE/?&_encoding=UTF8&tag=maykbrito0a-20&linkCode=ur2&linkId=8e2c5dc33eb897f15f2d219731c1555d&camp=1789&creative=9325",
   },
   {
     name: "Monitor LG 21.5 LED Full HD",
     imgSrc:
       "https://m.media-amazon.com/images/I/41Ib3U3BlUL.__AC_SX300_SY300_QL70_ML2_.jpg",
-    url: "https://www.amazon.com.br/Monitor-LG-LED-Full-21-5/dp/B07ML9239T/",
+    url: "https://www.amazon.com.br/Monitor-LG-LED-Full-21-5/dp/B07ML9239T/?&_encoding=UTF8&tag=maykbrito0a-20&linkCode=ur2&linkId=ce0993ec7578d616c94a4db5cbd6b5d5&camp=1789&creative=9325",
   },
   {
-    name: "SUPORTE MICROFONE P/GRAVAÇÃO",
+    name: "Suporte Microfone",
     imgSrc:
       "https://m.media-amazon.com/images/I/51S0NNSDNTL.__AC_SX300_SY300_QL70_ML2_.jpg",
-    url: "https://www.amazon.com.br/Knup-SUPORTE-MICROFONE-P-GRAVA%C3%87%C3%83O/dp/B08B459NF9/",
+    url: "https://www.amazon.com.br/Knup-SUPORTE-MICROFONE-P-GRAVA%25C3%2587%25C3%2583O/dp/B08B459NF9/?&_encoding=UTF8&tag=maykbrito0a-20&linkCode=ur2&linkId=a8b37fb50199279ea0ea59f57019be68&camp=1789&creative=9325",
   },
   {
-    name: "Teclado sem fio Logitech MX Keys S",
+    name: "Teclado Logitech MX Keys S",
     imgSrc:
       "https://m.media-amazon.com/images/I/71Tjmco5WRL.__AC_SX300_SY300_QL70_ML2_.jpg",
-    url: "https://www.amazon.com.br/Logitech-Silencioso-Program%C3%A1veis-Inteligente-Recarreg%C3%A1vel/dp/B0C6K19YBH/",
+    url: "https://www.amazon.com.br/Logitech-Silencioso-Program%25C3%25A1veis-Inteligente-Recarreg%25C3%25A1vel/dp/B0C6K19YBH/?&_encoding=UTF8&tag=maykbrito0a-20&linkCode=ur2&linkId=d3443a90ffe1f3009379610745c3eccf&camp=1789&creative=9325",
   },
   {
-    name: "Logitech G604 Lightspeed",
+    name: "Mouse Logitech G604 Lightspeed",
     imgSrc:
       "https://m.media-amazon.com/images/I/61QMAJc-TDL.__AC_SY300_SX300_QL70_ML2_.jpg",
-    url: "https://www.amazon.com.br/Logitech-G604-Lightspeed-Kabellose-Gaming-Maus/dp/B07W8RKK5R/",
+    url: "https://www.amazon.com.br/Logitech-G604-Lightspeed-Kabellose-Gaming-Maus/dp/B07W8RKK5R/?&_encoding=UTF8&tag=maykbrito0a-20&linkCode=ur2&linkId=9883d1ea70923bce36c35e6c43cd48d3&camp=1789&creative=9325",
   },
   {
-    name: "Apple notebook MacBook Pro 16 pol, M1 Pro MAX, 64GB, 1TB",
+    name: "MacBook Pro M1 Pro MAX",
     imgSrc:
       "https://m.media-amazon.com/images/I/61aUBxqc5PL.__AC_SY445_SX342_QL70_ML2_.jpg",
-    url: "https://www.amazon.com.br/Apple-MacBook-16-polegadas-Processador-GPU-16%E2%80%91core/dp/B09L5969NZ/",
+    url: "https://www.amazon.com.br/Apple-MacBook-16-polegadas-Processador-GPU-16%25E2%2580%2591core/dp/B09L5969NZ/?&_encoding=UTF8&tag=maykbrito0a-20&linkCode=ur2&linkId=d9952142aedaf356c80fcffd00c8dada&camp=1789&creative=9325",
   },
 ];
