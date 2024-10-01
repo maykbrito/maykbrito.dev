c9d446d89e368931e3c548022a3393ee2f0595cf
Mayk Brito <maykbrito@gmail.com>
2024-10-01 15:53:23 -0300

    another removng

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import svelte from '@astrojs/svelte'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
-import netlify from '@astrojs/netlify'
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maykbrito.dev',
